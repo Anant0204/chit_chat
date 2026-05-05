dceb94d16a7401cd655bc6ea8395dd5da1c545e5
Anant <gawdeanant4389@gmail.com>
2026-05-05 17:29:38 +0530

    resolve port no.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -8,6 +8,8 @@ import cloudinary from "../lib/cloudinary.js";
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
 
+  console.log(fullName, email, password);
+
   try {
     if (!fullName || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
@@ -51,7 +53,7 @@ export const signup = async (req, res) => {
         await sendWelcomeEmail(
           savedUser.email,
           savedUser.fullName,
-          ENV.CLIENT_URL
+          ENV.CLIENT_URL,
         );
       } catch (error) {
         console.error("Failed to send welcome email:", error);
@@ -112,7 +114,7 @@ export const updateProfile = async (req, res) => {
     const updatedUser = await User.findByIdAndUpdate(
       userId,
       { profilePic: uploadResponse.secure_url },
-      { new: true }
+      { new: true },
     );
 
     res.status(200).json(updatedUser);
